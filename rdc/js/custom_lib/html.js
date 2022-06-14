dd3ed856ab4721924933ce2712e1371ba46f39ab
Charilaos Papamatthaiou <haris.papamattheu@gmail.com>
2022-06-14 15:18:10 +0300

    Finish Html5Editor Page

--- rdc/js/custom_lib/html.js
+++ rdc/js/custom_lib/html.js
@@ -2,9 +2,12 @@ var select = function (elementSelectorCSS, AllElements) {
     elementSelectorCSS.trim();
     return (AllElements) ? document.querySelectorAll(elementSelectorCSS) : document.querySelector(elementSelectorCSS);
 };
-
 function htmlEntities(str) {
-    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+    return String(str)
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;');
 }
 
 (function(global, x){
@@ -12,21 +15,37 @@ function htmlEntities(str) {
     var saveButton        = select('#save');
     var codeField         = select('#code');
     var searchMessageArea = select('.message');
-    var Html5Field        = select('#html');
+    var Html5ShortDescr   = select('#htmlShort');
+    var Html5LongDescr    = select('#htmlLong');
     var productId         = 0;
     var getMtrl = function (code){
-        var ds=x.GETSQLDATASET("SELECT TOP 1 * FROM Mtrl WHERE COMPANY="+x.SYS.COMPANY+" AND Code='"+code+"';");
-        if (ds.RECORDCOUNT>0) {
-            return ds.Mtrl;
+        try{
+            var ds=x.GETSQLDATASET("SELECT TOP 1 * FROM Mtrl WHERE COMPANY="+x.SYS.COMPANY+" AND Code='"+code+"';");
+            if (ds.RECORDCOUNT>0) {
+                return ds.Mtrl;
+            }
+        }
+        catch (e) {
+            return e.message;
+        }
+        return 0;
+    };
+    var getHTML5 = function (id, HtmlField){
+        try{
+            var ds=x.GETSQLDATASET("SELECT "+ HtmlField +" AS Html FROM Mtrl WHERE SODTYPE=51 AND COMPANY="+x.SYS.COMPANY+" AND Mtrl="+id+";");
+            if (ds.RECORDCOUNT>0) {
+                return ds.html;
+            }
+        }
+        catch (e) {
+            return e.message;
         }
         return 0;
     };
     var updateHTML5Mtrl = function(id, HtmlField, HtmlText ){
         try{
-            alert(HtmlText.replace("'","''",'g'));
-            var sql = "UPDATE Mtrl SET "+ HtmlField +"='"+HtmlText.replace("'","''",'g')+"' WHERE SODTYPE=51 AND COMPANY="+x.SYS.COMPANY+" AND Mtrl="+id+";";
-            alert(sql);
-            x.RUNSQL(sql);
+            var sql = "UPDATE Mtrl SET "+ HtmlField +"=:1 WHERE SODTYPE=51 AND COMPANY="+x.SYS.COMPANY+" AND Mtrl="+id+";";
+            x.RUNSQL(sql, htmlEntities(HtmlText));
             searchMessageArea.textContent = 'Html Field Updated to Mtrl'+id;
             return 1;
         }
@@ -36,20 +55,23 @@ function htmlEntities(str) {
 
     };
     var checkCodeField = function (){
-        if (searchMessageArea.textContent.length>1){
-            searchMessageArea.textContent = '';
-            searchMessageArea.classList.remove('alert-danger');
-        }
         if(codeField.value.length === 0) {
             searchMessageArea.textContent = 'The code input must not be empty';
             searchMessageArea.classList.add('alert-danger');
             searchMtrlButton.classList.add('disabled');
+            saveButton.classList.add('disabled');
+        }
+        else if (searchMessageArea.textContent.length>1){
+            searchMessageArea.textContent = '';
+            searchMessageArea.classList.remove('alert-danger');
+            searchMessageArea.classList.remove('alert-success');
         } else {
             searchMtrlButton.classList.remove('disabled');
+            saveButton.classList.remove('disabled');
         }
     };
     var lookInsideS1 = function (evt) {
-        if( searchMtrlButton.classList.contains('disabled')){
+        if(searchMtrlButton.classList.contains('disabled')){
             checkCodeField();
         }
         else {
@@ -58,7 +80,10 @@ function htmlEntities(str) {
               productId = getMtrl(codeField.value);
               if (parseInt(productId)>0) {
                   searchMessageArea.classList.add('alert-success');
-                  searchMessageArea.textContent = 'i located the item in SoftOne with Mtrl='+productId;
+                  searchMessageArea.textContent = 'Item Located in SoftOne with Mtrl='+productId;
+                  var y = Object.getOwnPropertyNames(Html5LongDescr)[1];
+                  Html5ShortDescr[y].wysihtml5.editor.setValue(getHTML5(productId,'CCC1007ShortDescr'));
+                  Html5LongDescr[y].wysihtml5.editor.setValue(getHTML5(productId,'CCC1007LongDescr'));
               }
               else {
                   searchMessageArea.classList.add('alert-danger');
@@ -68,16 +93,31 @@ function htmlEntities(str) {
         }
     };
     var saveHTMLToDB = function(evt) {
-        if( searchMtrlButton.classList.contains('disabled')){
+        if(searchMtrlButton.classList.contains('disabled') || saveButton.classList.contains('disabled') ){
             checkCodeField();
         }
         else{
             evt.preventDefault();
-            var xe = updateHTML5Mtrl(productId, 'CCC1007ShortDescr', Html5Field.value.toString());
-            alert(xe);
+            if (productId === 0){
+                checkCodeField();
+                searchMessageArea.classList.add('alert-danger');
+                searchMessageArea.textContent = "The Html wasn't saved because sku doesn't exist.";
+            }
+            else{
+                var updateShort = updateHTML5Mtrl(productId, 'CCC1007ShortDescr', Html5ShortDescr.value.toString());
+                var updateLong = updateHTML5Mtrl(productId, 'CCC1007LongDescr', Html5LongDescr.value.toString());
+                if (updateShort === 1 || updateLong === 1) {
+                    searchMessageArea.classList.add('alert-success');
+                    searchMessageArea.textContent = "The Html was saved on product's view";
+                }
+                else{
+                    checkCodeField();
+                    searchMessageArea.classList.add('alert-danger');
+                    searchMessageArea.textContent = "The Html wasn't saved.";
+                }
+            }
         }
     };
-
     document.addEventListener("DOMContentLoaded", function (){
         codeField.addEventListener('blur',checkCodeField);
         searchMtrlButton.addEventListener("click", lookInsideS1);
--- rdc/js/jquery_lib/script.js
+++ rdc/js/jquery_lib/script.js
@@ -1,4 +1,5 @@
 /* jQuery Html5 CSS3 Template wait for document to load */
 $(document).ready(function () {
-
+    $('#htmlShort').wysihtml5();
+    $('#htmlLong').wysihtml5();
 });
